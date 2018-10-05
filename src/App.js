c022f0bfbd4d39b69d38641486b24ad1c3407dcd
Patryk Karwowski <vb.karwowski@gmail.com>
2018-10-05 08:16:54 +0100

    folder structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import LoginPage from './components/loginpage/login.page';
+
+class App extends Component {
+  render() {
+    return (
+      <LoginPage/>
+    );
+  }
+}
+
+export default App;
